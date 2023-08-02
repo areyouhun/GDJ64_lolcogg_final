d011fe6acf7a2d41cf5b4ef1aa8d60d77bbd4ca8
hyunzero0 <gusduddl486@naver.com>
2023-08-02 16:09:04 +0900

    feat(qna): 로그인 인터셉터 구현

--- src/main/java/gg/lolco/common/interceptor/LoginChecker.java
+++ src/main/java/gg/lolco/common/interceptor/LoginChecker.java
@@ -2,9 +2,12 @@ package gg.lolco.common.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import gg.lolco.model.vo.Member;
 
 @Component
 //@Slf4j
@@ -14,15 +17,15 @@ public class LoginChecker implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception 
 	{
-//		HttpSession session = request.getSession();
-//		Member loginMember = (Member) session.getAttribute("loginMember");
-//		
-//		if(loginMember == null) {
-//			request.setAttribute("msg", "로그인 후 이용할 수 있는 서비스 입니다.");
-//			request.setAttribute("loc", "/");
-//			request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
-//			return false;
-//		}
+		HttpSession session = request.getSession();
+		Member loginMember = (Member) session.getAttribute("loginMember");
+		
+		if(loginMember == null) {
+			request.setAttribute("msg", "로그인 후 이용할 수 있는 서비스입니다.");
+			request.setAttribute("loc", "/member/loginPage");
+			request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
+			return false;
+		}
 		return true;
 	}
 
