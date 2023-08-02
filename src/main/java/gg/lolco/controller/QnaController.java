15aa7043d52b8f96fc03591ecff357912f852d59
hyunzero0 <gusduddl486@naver.com>
2023-08-02 15:46:33 +0900

    feat(qna): qna 글, 댓글 조회 구현

--- src/main/java/gg/lolco/controller/QnaController.java
+++ src/main/java/gg/lolco/controller/QnaController.java
@@ -1,20 +1,31 @@
 package gg.lolco.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import gg.lolco.common.PageFactory;
 import gg.lolco.model.service.QnaService;
 import gg.lolco.model.vo.QaBoard;
+import gg.lolco.model.vo.QaBoardComment;
+import gg.lolco.model.vo.QaBoardFile;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -40,27 +51,46 @@ public class QnaController {
 		return "qna/qnaList";
 	}
 	
-//	qna 글 조회
+//	qna 글, 댓글 조회
 	@RequestMapping("/qnaView")
 	public String qnaView(Model m, int no) {
 		QaBoard qb = service.qnaView(no);
-		
+		List<QaBoardComment> qbc = service.selectCommentAll(no);
+
 		m.addAttribute("qb", qb);
+		m.addAttribute("qbc", qbc);
 		return "qna/qnaView";
 	}
 	
-//	qna 등록
-	@PostMapping("/insertQnaEnd")
-	public String insertQna(QaBoard qb, Model m, MultipartFile[] qaFile, HttpSession session) {
-		log.info("qbqbqbqbqb{}", qb);
-		log.info("upFileupFileupFile{}", qaFile);
+//	qna 삭제
+	@RequestMapping("/deleteQna")
+	public String deleteQna(int no, Model m) {
+		int result = service.deleteQna(no);
 		
-		// 절대경로
-		String path = session.getServletContext().getRealPath("/resources/upload/qna/");
+		String loc = "/qna/qnaList";
+		m.addAttribute("loc", loc);
+		if(result > 0) m.addAttribute("msg", "문의글 삭제 완료");
+		else m.addAttribute("msg", "문의글 삭제 실패");
 		
-		
-		
-		return "redirect:/qna/qnaList";
+		return "common/msg";
 	}
 	
+//	qna 댓글 등록
+	@PostMapping("/insertComment")
+	@ResponseBody
+	public List<QaBoardComment> insertComment(@RequestParam Map param) {
+		int qaNo = Integer.parseInt((String.valueOf(param.get("qaNo"))));
+		
+		// 댓글 등록
+		int result = service.insertComment(param);
+		
+		if(result > 0) {
+			// 성공 -> 댓글목록 가져와 리턴
+			List<QaBoardComment> qbc = service.selectCommentAll(qaNo);
+			return qbc;
+		} else {
+			return null;
+		}
+	}
+
 }
