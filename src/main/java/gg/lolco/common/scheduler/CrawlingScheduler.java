026b4d1100c634c3243c0e6883e0ec4b951fe51a
movementje <movementje@gmail.com>
2023-08-03 15:23:21 +0900

    fix(schedule): 스케쥴러 cron 오전 1시에 실행 설정

--- src/main/java/gg/lolco/common/scheduler/CrawlingScheduler.java
+++ src/main/java/gg/lolco/common/scheduler/CrawlingScheduler.java
@@ -39,7 +39,7 @@ public class CrawlingScheduler {
 		this.context = context;
 	}
 	
-	@Scheduled(fixedDelay=50000)
+	@Scheduled(cron = "0 0 1 * * ?")
 	public void catchExceptionMethod() {
 		try {
 			getPlayerData();
