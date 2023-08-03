d1ffe41806c4a5890175c170627b5741831caa95
movementje <movementje@gmail.com>
2023-08-03 14:50:09 +0900

    fix(schedule): Controller 삭제 및 중복 파일 방지

--- src/main/java/gg/lolco/common/scheduler/CrawlingScheduler.java
+++ src/main/java/gg/lolco/common/scheduler/CrawlingScheduler.java
@@ -1,6 +1,7 @@
 package gg.lolco.common.scheduler;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
@@ -18,12 +19,12 @@ import org.jsoup.select.Elements;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import gg.lolco.controller.SchedulerController;
+import gg.lolco.model.service.SchedulerService;
 
 @Component
 public class CrawlingScheduler {
 	
-	private final SchedulerController controller;
+	private final SchedulerService service;
 	private final ServletContext context;
 	
 	private final Map<String, String> PARSE_TEAM_NAME = Map.of(
@@ -33,12 +34,12 @@ public class CrawlingScheduler {
 			"KT Rolster", "KT", "T1", "T1",
 			"Liiv SANDBOX", "LSB", "Kwangdong Freecs", "KDF");
 	
-	public CrawlingScheduler(SchedulerController controller, ServletContext context) {
-		this.controller = controller;
+	public CrawlingScheduler(SchedulerService controller, ServletContext context) {
+		this.service = controller;
 		this.context = context;
 	}
 	
-	@Scheduled(fixedDelay=3600000)
+	@Scheduled(fixedDelay=5000)
 	public void catchExceptionMethod() {
 		try {
 			getPlayerData();
@@ -49,7 +50,6 @@ public class CrawlingScheduler {
 	}
 	
 	private void getPlayerData() throws IOException {
-		
 		List<Map<String, String>> playerData = new ArrayList<>();
 		
 		// 전체선수 페이지
@@ -137,20 +137,17 @@ public class CrawlingScheduler {
 				if(count > 4) break;
 				else bw.append("\n");
 			} if(bw != null) bw.close();
-		} controller.updatePlayerFile(playerData);
+		} service.updatePlayerFile(playerData);
 	}
 	
 	private void getMatchData() throws IOException {
 		List<Map<String, String>> matchData = new ArrayList<>();
 		
-		// 경기일정 페이지
-		Document doc = Jsoup.connect("https://gol.gg/tournament/tournament-matchlist/LCK%20Spring%202023/").get();
-		
-		List<String> matchTypeLink = doc.select("#gameMenuToggler .navbar-nav li a").eachAttr("href");
+		List<String> matchTypeLink = List.of(
+				"LCK%20Spring%202023/", "LCK%20Spring%20Playoffs%202023/", "LCK%20Summer%202023/");
 		
 		for(String link : matchTypeLink) {
-			Document matchListDoc = Jsoup.connect("https://gol.gg" + link.substring(2).replace("stats", "matchlist")).get();
-			
+			Document matchListDoc = Jsoup.connect("https://gol.gg/tournament/tournament-matchlist/" + link).get();
 			Elements tableRow = matchListDoc.select("table tbody tr");
 			
 			int tableRowSize = tableRow.select(".text-left a").size();
@@ -214,151 +211,62 @@ public class CrawlingScheduler {
 					
 					matchData.add(saveMap);
 					
-					for(int j=0; j<totalGameSetNum; j++) {
-						BufferedWriter bw = 
-								new BufferedWriter(new FileWriter(context.getRealPath("/resources/csv/match/") + fileName, true));
-
-						// 세트 구분 숫자
-						String divisionGameSet = gameSetResultUrl
-								.substring(gameSetResultUrl.indexOf("/page-game/")-5, gameSetResultUrl.indexOf("/page-game/"));
-						
-						// 다음 세트 숫자
-						int gameSetNum = Integer.parseInt(divisionGameSet) + j;
-						
-						
-						// 결과요약 페이지
-						Document gameInfoDoc = Jsoup.connect("https://" + gameInfoPath).get();
-						
-						// 결과정보 배열
-						String[] playerArr = gameInfoDoc.select(".completestats tbody tr").get(0).text()
-								.replace("Player ", "").split(" ");
-						String[] killArr = gameInfoDoc.select(".completestats tbody tr").get(3).text()
-								.replace("Kills ", "").split(" ");
-						String[] deathArr = gameInfoDoc.select(".completestats tbody tr").get(4).text()
-								.replace("Deaths ", "").split(" ");
-						String[] assistArr = gameInfoDoc.select(".completestats tbody tr").get(5).text()
-								.replace("Assists ", "").split(" ");
-						String[] csArr = gameInfoDoc.select(".completestats tbody tr").get(7).text().replace("CS ", "")
-								.split(" ");
-						String[] cpmArr = gameInfoDoc.select(".completestats tbody tr").get(10).text()
-								.replace("CSM ", "").split(" ");
-						String[] goldArr = gameInfoDoc.select(".completestats tbody tr").get(11).text()
-								.replace("Golds ", "").split(" ");
-						String[] damageArr = gameInfoDoc.select(".completestats tbody tr").get(24).text()
-								.replace("Total damage to Champion ", "").split(" ");
-						
-						// 세트 결과 페이지 주소
-						String gameSetPath = gameSetResultUrl.replace(divisionGameSet, String.valueOf(gameSetNum));
-						
-						// 세트 결과 페이지 
-						Document gameSetDoc = Jsoup.connect("https://" + gameSetPath).get();
-						
-						// 밴픽
-						List<String> banpickSrcList = gameSetDoc.select("a.black_link img").eachAttr("src");
-						
-						List<String> banpickList = new ArrayList<>();
-						
-						for(String banpick : banpickSrcList) {
-							if(!gameSetDoc.select("img[alt=No ban]").isEmpty()) {
-								banpickList.add("No ban");
-							}
-							banpickList.add(banpick.substring(banpick.indexOf("icon/") + 5, banpick.indexOf(".png")));
-						}
-						
-						// 챔피언
-						List<String> championSrcList = 
-								gameSetDoc.select(".playersInfosLine td[style=white-space:nowrap] img[src*=champions]").eachAttr("src");
-						
-						List<String> championList = new ArrayList<>();
-						
-						for(String champion : championSrcList) {
-							championList.add(champion.substring(champion.indexOf("icon/") + 5, champion.indexOf(".png")));
-						}
-
-						// 스펠
-						List<String> spellSrcList = gameSetDoc.select(".playersInfosLine img[alt=Summoner spell]").eachAttr("src");
-						
-						List<String> spellList = new ArrayList<>();
-						
-						for(String spell : spellSrcList) {
-							spellList.add(spell.substring(spell.indexOf("Summoner"), spell.indexOf(".png")).replace("Summoner", ""));
-						}
-						
-						// 각 챔피언의 스펠
-						Map<String, String[]> championSpellMap = new HashMap<>();
-						
-						for(int k=0; k<championList.size(); k++) {
-							String[] spells = {spellList.get(k * 2), spellList.get(k * 2 + 1)};
-							championSpellMap.put(championList.get(k), spells);
-						}
-						
-						// 아이템
-						Map<String, List<String>> itemMap = new HashMap<>();
-						
-						for(int k=0; k<10; k++) {
-							List<String> itemList = new ArrayList<>();
+					File csvFile = new File(context.getRealPath("/resources/csv/match/") + fileName);
+					
+					if(!csvFile.exists()) {
+						for(int j=0; j<totalGameSetNum; j++) {
+							BufferedWriter bw = 
+									new BufferedWriter(new FileWriter(context.getRealPath("/resources/csv/match/") + fileName, true));
 							
-							List<String> itemSrcList = gameSetDoc.select(".playersInfosLine tr td[colspan=3]").get(k)
-									.select("img[alt=Items]").eachAttr("src");
 							
-							for(String item : itemSrcList) {
-								itemList.add(item.substring(item.indexOf("item"), item.indexOf(".png")).replace("item/", ""));
-							}
+							// 세트 구분 숫자
+							String divisionGameSet = gameSetResultUrl
+									.substring(gameSetResultUrl.indexOf("/page-game/")-5, gameSetResultUrl.indexOf("/page-game/"));
 							
-							itemMap.put(championList.get(k), itemList);
-						}
-						
-						// 팀 구분
-						String[] lineArr = {".blue", ".red"};
-						
-						for(String line : lineArr) {
-							// 팀 정보
-							boolean isWinnerTeam = gameSetDoc.select(line + "-line-header").text().contains("WIN");
-							String teamName = gameSetDoc.select(line + "-line-header a").text();
-							String teamTotalKill = gameSetDoc.select(line + "_line").tagName("img").first().text();
-							String teamTotalTour = gameSetDoc.select(line + "_line").tagName("img").get(1).text();
-							String teamTotalDragon = gameSetDoc.select(line + "_line").tagName("img").get(2).text();
-							String teamTotalBaron = gameSetDoc.select(line + "_line").tagName("img").get(3).text();
-							int teamTotalHerald = gameSetDoc.select(line + "_action img[alt=Rift Herald]").size();
+							// 다음 세트 숫자
+							int gameSetNum = Integer.parseInt(divisionGameSet) + j;
 							
-							if(isWinnerTeam) {
-								bw.append(PARSE_TEAM_NAME.get(teamName) + ",VICTORY," + teamTotalKill + "," + teamTotalTour 
-										+ "," + teamTotalDragon + "," + teamTotalBaron + "," + teamTotalHerald + "\n");
-							}else {
-								bw.append(PARSE_TEAM_NAME.get(teamName) + ",DEFEAT," + teamTotalKill + "," + teamTotalTour 
-										+ "," + teamTotalDragon + "," + teamTotalBaron + "," + teamTotalHerald + "\n");
-							}
+							
+							// 결과요약 페이지
+							Document gameInfoDoc = Jsoup.connect("https://" + gameInfoPath).get();
+							
+							// 결과정보 배열
+							String[] playerArr = gameInfoDoc.select(".completestats tbody tr").get(0).text()
+									.replace("Player ", "").split(" ");
+							String[] killArr = gameInfoDoc.select(".completestats tbody tr").get(3).text()
+									.replace("Kills ", "").split(" ");
+							String[] deathArr = gameInfoDoc.select(".completestats tbody tr").get(4).text()
+									.replace("Deaths ", "").split(" ");
+							String[] assistArr = gameInfoDoc.select(".completestats tbody tr").get(5).text()
+									.replace("Assists ", "").split(" ");
+							String[] csArr = gameInfoDoc.select(".completestats tbody tr").get(7).text().replace("CS ", "")
+									.split(" ");
+							String[] cpmArr = gameInfoDoc.select(".completestats tbody tr").get(10).text()
+									.replace("CSM ", "").split(" ");
+							String[] goldArr = gameInfoDoc.select(".completestats tbody tr").get(11).text()
+									.replace("Golds ", "").split(" ");
+							String[] damageArr = gameInfoDoc.select(".completestats tbody tr").get(24).text()
+									.replace("Total damage to Champion ", "").split(" ");
+							
+							// 세트 결과 페이지 주소
+							String gameSetPath = gameSetResultUrl.replace(divisionGameSet, String.valueOf(gameSetNum));
+							
+							// 세트 결과 페이지 
+							Document gameSetDoc = Jsoup.connect("https://" + gameSetPath).get();
 							
 							// 밴픽
-							for(int k=0; k<5; k++) {
-								if(line.equals(".blue")) {
-									bw.append(banpickList.get(k).substring(0, 1).toUpperCase() + banpickList.get(k).substring(1));
-									bw.append(k < 4 ? ",\n" : "\n");
-								}else {
-									bw.append(banpickList.get(k + 5).substring(0, 1).toUpperCase() + banpickList.get(k).substring(1));
-									bw.append(k < 4 ? ",\n" : "\n");
+							List<String> banpickSrcList = gameSetDoc.select("a.black_link img").eachAttr("src");
+							
+							List<String> banpickList = new ArrayList<>();
+							
+							for(String banpick : banpickSrcList) {
+								if(!gameSetDoc.select("img[alt=No ban]").isEmpty()) {
+									banpickList.add("No ban");
 								}
-							}
-						}
-						
-						for(int k=0; k<championList.size(); k++) {
-							int kill = Integer.parseInt(killArr[k]);
-							int death = Integer.parseInt(deathArr[k]);
-							int assist = Integer.parseInt(assistArr[k]);
-							
-							double avgNum = (k == 0) ? (kill + assist) : ((double)(kill + assist) / death);
-							
-							String avg = new DecimalFormat("0.0").format(avgNum);
-							
-							bw.append(playerArr[k] + "\n");
-							
-							String championName = championList.get(k);
-							
-							String[] spell = championSpellMap.get(championName);
-							
-							List<String> itemList = itemMap.get(championName);
-							
-							switch(championName) {
+								
+								String championName = banpick.substring(banpick.indexOf("icon/") + 5, banpick.indexOf(".png"));
+								
+								switch(championName) {
 								case "Renata Glasc": 
 									championName = "Renata"; 
 									break;
@@ -368,8 +276,121 @@ public class CrawlingScheduler {
 								case "LeBlanc":
 									championName = "Leblanc";
 									break;
-								case "KaiSa":
-									championName = "Kaisa";
+								case "KhaZix":
+									championName = "Khazix";
+									break;
+								default:
+									if(championName.contains("'")) championName = championName.replace("'", "");
+									
+									if(championName.contains(" ")) championName = championName.replace(" ", "");
+								}
+								banpickList.add(championName);
+							}
+							
+							// 챔피언
+							List<String> championSrcList = 
+									gameSetDoc.select(".playersInfosLine td[style=white-space:nowrap] img[src*=champions]").eachAttr("src");
+							
+							List<String> championList = new ArrayList<>();
+							
+							for(String champion : championSrcList) {
+								championList.add(champion.substring(champion.indexOf("icon/") + 5, champion.indexOf(".png")));
+							}
+							
+							// 스펠
+							List<String> spellSrcList = gameSetDoc.select(".playersInfosLine img[alt=Summoner spell]").eachAttr("src");
+							
+							List<String> spellList = new ArrayList<>();
+							
+							for(String spell : spellSrcList) {
+								spellList.add(spell.substring(spell.indexOf("Summoner"), spell.indexOf(".png")).replace("Summoner", ""));
+							}
+							
+							// 각 챔피언의 스펠
+							Map<String, String[]> championSpellMap = new HashMap<>();
+							
+							for(int k=0; k<championList.size(); k++) {
+								String[] spells = {spellList.get(k * 2), spellList.get(k * 2 + 1)};
+								championSpellMap.put(championList.get(k), spells);
+							}
+							
+							// 아이템
+							Map<String, List<String>> itemMap = new HashMap<>();
+							
+							for(int k=0; k<10; k++) {
+								List<String> itemList = new ArrayList<>();
+								
+								List<String> itemSrcList = gameSetDoc.select(".playersInfosLine tr td[colspan=3]").get(k)
+										.select("img[alt=Items]").eachAttr("src");
+								
+								for(String item : itemSrcList) {
+									if(item.contains("item")) {
+										itemList.add(item.substring(item.indexOf("item"), item.indexOf(".png")).replace("item/", ""));
+									}
+								}
+								
+								itemMap.put(championList.get(k), itemList);
+							}
+							
+							// 팀 구분
+							String[] lineArr = {".blue", ".red"};
+							
+							for(String line : lineArr) {
+								// 팀 정보
+								boolean isWinnerTeam = gameSetDoc.select(line + "-line-header").text().contains("WIN");
+								String teamName = gameSetDoc.select(line + "-line-header a").text();
+								String teamTotalKill = gameSetDoc.select(line + "_line").tagName("img").first().text();
+								String teamTotalTour = gameSetDoc.select(line + "_line").tagName("img").get(1).text();
+								String teamTotalDragon = gameSetDoc.select(line + "_line").tagName("img").get(2).text();
+								String teamTotalBaron = gameSetDoc.select(line + "_line").tagName("img").get(3).text();
+								int teamTotalHerald = gameSetDoc.select(line + "_action img[alt=Rift Herald]").size();
+								
+								if(isWinnerTeam) {
+									bw.append(PARSE_TEAM_NAME.get(teamName) + ",VICTORY," + teamTotalKill + "," + teamTotalTour 
+											+ "," + teamTotalDragon + "," + teamTotalBaron + "," + teamTotalHerald + "\n");
+								}else {
+									bw.append(PARSE_TEAM_NAME.get(teamName) + ",DEFEAT," + teamTotalKill + "," + teamTotalTour 
+											+ "," + teamTotalDragon + "," + teamTotalBaron + "," + teamTotalHerald + "\n");
+								}
+								
+								// 밴픽
+								for(int k=0; k<5; k++) {
+									if(line.equals(".blue")) {
+										bw.append(banpickList.get(k));
+										bw.append(k < 4 ? "," : "\n");
+									}else {
+										bw.append(banpickList.get(k + 5));
+										bw.append(k < 4 ? "," : "\n");
+									}
+								}
+							}
+							
+							for(int k=0; k<championList.size(); k++) {
+								int kill = Integer.parseInt(killArr[k]);
+								int death = Integer.parseInt(deathArr[k]);
+								int assist = Integer.parseInt(assistArr[k]);
+								
+								double avgNum = (death == 0) ? (kill + assist) : ((double)(kill + assist) / death);
+								
+								String avg = new DecimalFormat("0.0").format(avgNum);
+								
+								bw.append(playerArr[k] + "\n");
+								
+								String championName = championList.get(k);
+								
+								String[] spell = championSpellMap.get(championName);
+								
+								List<String> itemList = itemMap.get(championName);
+								
+								switch(championName) {
+								case "Renata Glasc": 
+									championName = "Renata"; 
+									break;
+								case "Wukong": 
+									championName = "MonkeyKing"; 
+									break;
+								case "LeBlanc":
+									championName = "Leblanc";
 									break;
 								case "KhaZix":
 									championName = "Khazix";
@@ -378,30 +399,32 @@ public class CrawlingScheduler {
 									if(championName.contains("'")) championName = championName.replace("'", "");
 									
 									if(championName.contains(" ")) championName = championName.replace(" ", "");
+								}
+								
+								bw.append(championName.substring(0, 1).toUpperCase() + championName.substring(1) + ",");
+								
+								bw.append(spell[0] + "," + spell[1] + ",");
+								
+								for(int m=0; m<itemList.size(); m++) {
+									bw.append(itemList.get(m));
+									bw.append(m == itemList.size() - 1 ? "\n" : ",");
+								}
+								
+								bw.append(killArr[k] + "/" + deathArr[k] + "/" + assistArr[k] + "," + avg + "," + csArr[k]
+										+ "," + cpmArr[k] + "," + goldArr[k] + "," + damageArr[k]);
+								
+								if(!(j == totalGameSetNum - 1) && k == 9) {
+									bw.append("\n|\n");
+								}else if(k < 9) {
+									bw.append("\n");
+								}
 							}
-							
-							bw.append(championName.substring(0, 1).toUpperCase() + championName.substring(1) + ",");
-							
-							bw.append(spell[0] + "," + spell[1] + ",");
-							
-							for(int m=0; m<itemList.size(); m++) {
-								bw.append(itemList.get(m));
-								bw.append(m == itemList.size() - 1 ? "\n" : ",");
-							}
-							
-							bw.append(killArr[k] + "/" + deathArr[k] + "/" + assistArr[k] + "," + avg + "," + csArr[k]
-									+ "," + cpmArr[k] + "," + goldArr[k] + "k," + damageArr[k] + "\n");
-							
-							if(!(j == totalGameSetNum - 1) && k == 9) {
-								bw.append("\n|\n");
-							}
+							if(bw != null) bw.close();
+							System.out.println(fileName + " 업데이트 되었습니다.");
 						}
-						if(bw != null) bw.close();
 					}
 				}
 			}				
-		}
-		controller.updateMatchFile(matchData);
+		} service.updateMatchFile(matchData);
 	}
-	
 }
