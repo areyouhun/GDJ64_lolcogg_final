a83cc0419070f5cf94576e651e955fdfe9240d15
hyunzero0 <gusduddl486@naver.com>
2023-08-07 09:32:16 +0900

    feat(mp): 보유이모티콘 vo 생성

---
+++ src/main/java/gg/lolco/model/vo/MemberEmoticon.java
@@ -0,0 +1,18 @@
+package gg.lolco.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberEmoticon {
+	private long meNo;
+	private String meEmail;
+	private long meEmoNo;
+	
+	private Emoticon emoticon;
+}
