e6862dacd32ea2fe0b1224f705a5f931f74a9864
jsjyyy02190@gmail.com <tkdwns219@naver.com>
2023-08-19 06:54:23 +0900

    modify

--- src/main/java/gg/lolco/controller/LoginController.java
+++ src/main/java/gg/lolco/controller/LoginController.java
@@ -122,7 +122,7 @@ public class LoginController {
 //		System.out.println(code);
 //		System.out.println(state);
 //		String redirectURI = URLEncoder.encode("http://localhost:7070/login/naverLogin", "UTF-8");
-		String redirectURI = URLEncoder.encode("http://14.36.141.71:10005/GDJ64_lolcogg_final/member/loginPage", "UTF-8");
+		String redirectURI = URLEncoder.encode("http://14.36.141.71:10005/GDJ64_lolcogg_final/login/naverLogin", "UTF-8");
 		String apiURL;
 		apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
 		apiURL += "client_id=" + CLIENT_ID;
